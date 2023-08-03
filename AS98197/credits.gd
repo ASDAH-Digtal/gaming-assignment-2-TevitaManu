cb6e7b43bbf5ab3ba8f7a27731b6d32993e2e2a5
tevita-man <tevita_man@student.asdah.school.nz>
2023-08-04 10:37:09 +1200

    Updating game
    Signed-off-by: tevita-man <tevita_man@student.asdah.school.nz>

---
+++ AS98197/credits.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the firs	
+
+
+
+
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://Start_Menu/start_menu.tscn")
